dd7aadc5c061f67b98c903d0ad566670951b92c8
Lukas Finne <lukas.ikvm1@gmail.com>
2026-05-22 17:26:45 +0200

    Add daisyui + Login component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Login from "../components/login";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -6,8 +7,8 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-2">
-      <p className="text-3xl">Text</p>
+    <div className="">
+      <Login/> 
     </div>
   );
 }
