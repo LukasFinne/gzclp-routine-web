bd359d22c8ca7080aecded6e89aee6a9e58c13ef
Lukas Finne <lukas.ikvm1@gmail.com>
2026-05-23 09:15:26 +0200

    Add useActionState

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,5 +1,5 @@
 import { signInWithEmailAndPassword } from "firebase/auth/web-extension";
-import React, { useState } from "react";
+import React, { useActionState, useState } from "react";
 import z from "zod";
 import { auth } from "../lib/firebase";
 
@@ -7,6 +7,11 @@ const createLoginSchema = z.object({
   email: z.email(),
   password: z.string().min(6),
 });
+
+type LoginState = {
+  success: boolean;
+  message: string | null;
+} | null;
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -20,24 +25,71 @@ const Login = () => {
     setPassword(event.target.value);
   };
 
-  const loginAction = async (formData: FormData) => {
-    const { email, password } = createLoginSchema.parse(
+  const loginAction = async (
+    prevState: LoginState, // The first argument is the state, NOT the form data
+    formData: FormData,
+  ): Promise<LoginState> => {
+    // Must return the same LoginState type
+
+    const validatedFields = createLoginSchema.safeParse(
       Object.fromEntries(formData),
     );
+
+    if (!validatedFields.success) {
+      return {
+        success: false,
+        message: "Invalid email or password.",
+      };
+    }
+
+    const { email, password } = validatedFields.data;
+
     try {
       await signInWithEmailAndPassword(auth, email, password);
-    } catch (err) {
-      console.log(err);
+      return { success: true, message: "Logged in successfully!" };
+    } catch (err: any) {
+      console.error(err);
+      return {
+        success: false,
+        message: "Invalid email or password.",
+      };
     }
   };
+
+  const [actionState, dispatch, isPending] = useActionState<
+    LoginState,
+    FormData
+  >(
+    loginAction,
+    null, // Initial state matches 'LoginState' because we included '| null'
+  );
   return (
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
-        <form action={loginAction} className="space-y-2">
+        {actionState?.message && (
+          <div role="alert" className="alert alert-warning">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 shrink-0 stroke-current"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              />
+            </svg>
+            <span>{actionState.message}</span>
+          </div>
+        )}
+        <form action={dispatch} className="space-y-2">
           {/*Like the article with comments and posts. use the composition pattern for email and password */}
           <InputWithLabel
             id="email"
             type="email"
+            disabled={isPending}
             value={email}
             onInputChange={handleEmailInput}
           >
@@ -46,13 +98,22 @@ const Login = () => {
           <InputWithLabel
             id="password"
             type="password"
+            disabled={isPending}
             value={password}
             onInputChange={handlePasswordInput}
           >
             Password
           </InputWithLabel>
-          <button type="submit" className="btn btn-neutral mt-4">
-            Login
+          <button
+            type="submit"
+            disabled={isPending}
+            className="btn btn-neutral mt-4 w-full"
+          >
+            {isPending ? (
+              <span className="loading loading-dots loading-md"></span>
+            ) : (
+              <p>Login</p>
+            )}
           </button>
         </form>
       </div>
@@ -65,6 +126,7 @@ interface InputWithLabelProps {
   id: string;
   value: string;
   type?: string; // Optional because you provided a default value ("text")
+  disabled?: boolean;
   onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   children: React.ReactNode; // Correct type for anything passed between component tags
 }
@@ -74,6 +136,7 @@ const InputWithLabel = ({
   id,
   value,
   type = "text",
+  disabled = false,
   onInputChange,
   children,
 }: InputWithLabelProps) => {
@@ -87,6 +150,7 @@ const InputWithLabel = ({
         className="input"
         name={id}
         id={id}
+        disabled={disabled}
         type={type}
         value={value}
         onChange={onInputChange}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Login from "../components/login";
+import { useUser } from "../lib/hooks";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const user = useUser()
   return (
     <div className="hero flex-1">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <Login />
+        {user ? 
+          <p>Logged in</p>
+          :
+          <Login />
+        }
       </div>
     </div>
   );
