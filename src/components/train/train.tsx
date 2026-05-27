1b03181e4d077fed0ad6f5deeb2a48d8b13c1aa9
Lukas Finne <lukas.ikvm1@gmail.com>
2026-05-27 16:43:35 +0200

    train UI WIP

--- src/components/train/train.tsx
+++ src/components/train/train.tsx
@@ -11,14 +11,32 @@ export const Train = ({ user }: { user: User }) => {
   const handleKeyRotation = () => {
     UpdateWorkoutKey(user, userWorkout.currentWorkout);
   };
-
+  // Tier rotation logic ->
+  // 1. tier 1 -> tier 2
+  // 2. tier 2 -> tier 3
+  // 3. tier 3 -> finish workout
+  // Behöver bara ha en failed och success button med sido effekt som next? 
   return (
     <>
-      <p>Train Component</p>
-      <Button style="btn btn-primary" name="rotateBtn" onClick={handleKeyRotation}>Rotate</Button>
-      <ul> 
-        <li>{currentWorkout?.name}</li>
-      </ul>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">{currentWorkout?.name}</h1>
+            <ul className="py-6">
+              <li>{currentWorkout?.tier1.name}</li>
+              <li>
+                {currentWorkout?.tier1.protocol.set} X{" "}
+                {currentWorkout?.tier1.protocol.reps}
+              </li>
+              <li>{currentWorkout?.tier1.weight}</li>
+            </ul>
+            <div className="w-full space-x-4 ">
+              <Button style="btn btn-secondary btn-xl sm:btn-md">Failed</Button>
+              <Button style="btn btn-primary btn-xl sm:btn-md">Success</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
