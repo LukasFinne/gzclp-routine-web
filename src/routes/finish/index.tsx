06fa73d8a30d3d179d2952fb15bcfa87660cd6eb
Lukas Finne <lukas.ikvm1@gmail.com>
2026-06-24 10:22:10 +0200

    Add check user authentication on finish route

--- src/routes/finish/index.tsx
+++ src/routes/finish/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, useLocation, Link } from "@tanstack/react-router";
+import { createFileRoute, redirect, useLocation, useRouteContext } from "@tanstack/react-router";
 import type { WorkoutData } from "../../lib/workout/workout";
 import { Finished } from "../../components/train/finish/finished";
 import { NotWorkoutData } from "../../components/train/finish/error";
@@ -11,9 +11,19 @@ declare module "@tanstack/react-router" {
 
 export const Route = createFileRoute("/finish/")({
   component: RouteComponent,
+  beforeLoad: ({ context }) => {
+    if (!context.user) {
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
+      throw redirect({ to: "/" });
+    }
+  },
 });
 
 function RouteComponent() {
+  const { user } = useRouteContext({ from: "/finish/" });
+  if (!user) {
+    return <p>Error</p>;
+  }
   const location = useLocation();
   const workout = location.state.workouts;
 
