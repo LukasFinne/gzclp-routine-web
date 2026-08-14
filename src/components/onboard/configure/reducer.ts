74e3551c1b5b4fbbe1c9ee67d59f0544e5a14367
Lukas Finne <lukas.ikvm1@gmail.com>
2026-08-14 15:16:46 +0200

    Add onboarding pagination

---
+++ src/components/onboard/configure/reducer.ts
@@ -0,0 +1,73 @@
+import type { Steps } from "./steps";
+
+export type Action =
+  | {
+      type: "CONFIGURE_INIT";
+    }
+  | {
+      type: "NEXT_STEP";
+    }
+  | {
+      type: "PREVIOUS_STEP";
+    };
+
+export interface State {
+  previousSteps: Steps[];
+  currentStep: Steps;
+}
+
+export const initialState: State = {
+  currentStep: "Day",
+  previousSteps: ["Day"],
+};
+
+const DayOrder: Record<Steps, Steps> = {
+  Day: "Weight",
+  Weight: "Protocol",
+  Protocol: "Finish",
+  Finish: "Finish",
+};
+
+const RotateDayOrder = (day: Steps) => {
+  return DayOrder[day];
+};
+
+export const configureReducer = (
+  state: State = initialState,
+  action: Action,
+): State => {
+  switch (action.type) {
+    case "CONFIGURE_INIT": {
+      return {
+        ...state,
+      };
+    }
+    case "NEXT_STEP": {
+      const current = state.currentStep;
+      const listOfPrevious = state.previousSteps;
+      listOfPrevious.push(RotateDayOrder(current));
+
+      return {
+        ...state,
+        previousSteps: listOfPrevious,
+        currentStep: RotateDayOrder(current),
+      };
+    }
+    case "PREVIOUS_STEP": {
+      const current = state.currentStep;
+      const listOfPrevious =
+        current != "Day"
+          ? state.previousSteps.filter((day) => day != current)
+          : state.previousSteps;
+
+      return {
+        ...state,
+        previousSteps: listOfPrevious,
+        currentStep: listOfPrevious.at(-1) ?? "Day",
+      };
+    }
+    default: {
+      throw new Error("Unexpected state in configure reducer");
+    }
+  }
+};
