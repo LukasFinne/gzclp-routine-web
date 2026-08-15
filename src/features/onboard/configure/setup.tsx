9d599ebeaafd2a16a51d02a1a7aa8f2482b1f92c
Lukas Finne <lukas.ikvm1@gmail.com>
2026-08-15 16:06:38 +0200

    Add Radio button for choosing workout day

--- src/features/onboard/configure/setup.tsx
+++ src/features/onboard/configure/setup.tsx
@@ -1,14 +1,54 @@
-import type { Steps } from "./steps"
+import type { DocumentId } from "../../../lib/workout/workout";
+import type { Steps } from "./steps";
 
 interface setupProps {
-  step: Steps
+  step: Steps;
 }
 
-export const Setup = ({step}: setupProps) => {
+const workoutDays: DocumentId[] = ["A1", "B1", "A2", "B2"];
 
+export const Setup = ({ step }: setupProps) => {
   return (
-    <div className="flex flex-col space-y-4 p-4 w-full">
-      <p>{step}</p>
-    </div>
-  )
-}
+    <div className="flex flex-col space-y-4 p-4 w-full">{StepsState[step]}</div>
+  );
+};
+
+const WorkoutDays = () => (
+  <div className="card bg-base-100 p-4 h-full">
+    {workoutDays.map((day) => (
+      <div className="pt-4">
+        {
+          day === "A1" ?
+            <div className="indicator w-full">
+              <span className="indicator-item indicator-center badge badge-accent">Default</span>
+              <label className="label w-full cursor-pointer justify-start gap-3 rounded-2xl border border-base-300 bg-base-200/50 p-4 transition-all duration-150 hover:bg-base-200 hover:border-base-content/20 has-checked:border-primary has-checked:bg-primary has-checked:text-primary-content has-checked:shadow-sm">
+                <input
+                  type="radio"
+                  name="radio-1"
+                  className="peer hidden"
+                  defaultChecked
+                />
+                <span className="label-text font-medium text-inherit">{day}</span>
+              </label>
+          </div>
+            :
+            <label className="label w-full cursor-pointer justify-start gap-3 rounded-2xl border border-base-300 bg-base-200/50 p-4 transition-all duration-150 hover:bg-base-200 hover:border-base-content/20 has-checked:border-primary has-checked:bg-primary has-checked:text-primary-content has-checked:shadow-sm">
+              <input
+                type="radio"
+                name="radio-1"
+                className="peer hidden"
+              />
+              <span className="label-text font-medium text-inherit">{day}</span>
+            </label>
+        }
+      </div>
+    ))}
+  </div>
+);
+
+const StepsState = {
+  Day: <WorkoutDays />,
+  Weight: <p>weight </p>,
+  Protocol: <p>protocol </p>,
+  Finish: <p>finish </p>,
+};
